6848f0ef70877de2ca1e352a6bc8b9b105c82b61
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-08-16 10:49:06 +0530

    url's placed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-   const  url = "https://food-delivery-app-10.onrender.com/"
+   const  url = "https://food-delivery-app-10.onrender.com"
               
   return (
     <div>
--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-10.onrender.com/'
+export const url = 'https://food-delivery-app-10.onrender.com'
